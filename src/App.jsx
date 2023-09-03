47a560088c1772de98bdb48456759b919c4cf488
Vilaphong <vilaphong.m@gmail.com>
2023-09-03 09:36:43 +0700

    dddd

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,27 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './pages/app/home'
-import { PublicRoutes } from './routes/publicRoute'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/app/home";
+import { PublicRoutes } from "./routes/publicRoute";
+import { AdminRoutes } from "./routes/adminRoute";
 
 const App = () => {
-
-
-    return (
-        <BrowserRouter >
-            <Routes>
-                {/* <Route path='/' element >
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path='/' element >
                 
                 </Route> */}
 
-                {PublicRoutes.map((route) => (
-                    <Route path={route.path} element={<route.element />} />
-                ))}
+        {PublicRoutes.map((route) => (
+          <Route path={route.path} element={<route.element />} />
+        ))}
 
+        {AdminRoutes.map((route) => (
+          <Route path={route.path} element={<route.element />} />
+        ))}
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-            </Routes>
-        </BrowserRouter>
-    )
-}
-
-export default App
+export default App;
